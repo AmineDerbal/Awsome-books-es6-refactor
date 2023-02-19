64f6ac97624dcb61720d13e51f6244a842218c3c
AmineD931 <aminedoran31@gmail.com>
2023-02-19 19:59:23 +0100

    get time from luxon library

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import { DateTime } from './node_modules/luxon/src/luxon.js';
 import Book from './modules/Book.js';
 import Library from './modules/Library.js';
 
@@ -12,35 +13,9 @@ const addForm = document.getElementById('add-form');
 const contact = document.getElementById('contact');
 const time = document.querySelector('.time');
 
-const getDaySuffix = (day) => {
-  if (day === 1 || day === 21 || day === 31) {
-    return 'st';
-  } if (day === 2 || day === 22) {
-    return 'nd';
-  } if (day === 3 || day === 23) {
-    return 'rd';
-  }
-  return 'th';
-};
-
-const getDatePeriod = (hours) => {
-  if (hours < 12) {
-    return 'am';
-  }
-  return 'pm';
-};
-
 const getTime = () => {
-  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-  const month = months[new Date().getMonth()];
-  const day = new Date().getDate();
-  const year = new Date().getFullYear();
-  const date = new Date();
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const seconds = date.getSeconds();
-  time.innerHTML = `${month} ${day}${getDaySuffix(day)} ${year}, ${hours}:${minutes}:${seconds} ${getDatePeriod(hours)}`;
+  const date = DateTime.now();
+  time.textContent = date.toLocaleString(DateTime.DATETIME_HUGE_WITH_SECONDS);
 };
 
 setInterval(getTime, 1000);
