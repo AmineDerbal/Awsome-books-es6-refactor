93a30862530209b18fd9e42c9c8959f463bb78ca
AmineD931 <aminedoran31@gmail.com>
2023-02-20 10:04:49 +0100

    change DateTime import path

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { DateTime } from './node_modules/luxon/src/luxon.js';
+import { DateTime } from './modules/luxon.js';
 import Book from './modules/Book.js';
 import Library from './modules/Library.js';
 
